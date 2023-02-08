bb6b5216f4853c1c8ecb90425c92e87344e31458
T.Ekaterina S <katerina.tokan@yandex.ru>
2023-02-08 14:46:49 +0300

    .

---
+++ src/main/java/me/tokan/coursework3/model/SockItem.java
@@ -0,0 +1,12 @@
+package me.tokan.coursework3.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class SockItem {
+
+	private Sock sock;
+	private int quantity;
+}
